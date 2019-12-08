c713128a98b7d67c6d40aabbcd687d5f5f5f198b
CrocoGab <crocogab@gmail.com>
2019-12-08 10:15:25 +0100

    Last Version v1.0
    Last Version v1.0

---
+++ AeroBot v1.0.py
@@ -0,0 +1,191 @@
+a=3
+n=1
+print("Ce bot a été codé par Gabriel Diyan merci de ne pas le réutiliser sans citer son auteur");
+print("Le bot est mit à jour chaque semaine RDV sur le repo Github afin de récupérer la derniere version");
+print("Version actuelle : v1.0");
+
+while a>2:
+  from imagesearch import *
+  pos = imagesearch("./test2.jpg", 0.5)
+  pos = imagesearch("./test2.jpg")
+  if pos[0] != -1:
+      click_image("test2.jpg", pos, "left", 0.2)
+
+
+  pos = imagesearch("./test3.jpg", 0.5)
+  pos = imagesearch("test3.jpg")
+  if pos[0] != -1:
+      click_image("test3.jpg", pos, "left", 0.2)
+
+
+  ppos = imagesearch("./test4.jpg", 0.5)
+  pos = imagesearch("test4.jpg")
+  if pos[0] != -1:
+      click_image("test4.jpg", pos, "left", 0.2)
+
+
+
+
+
+  pos = imagesearch("./test5.jpg", 0.5)
+  pos = imagesearch("test5.jpg")
+  if pos[0] != -1:
+      click_image("test5.jpg", pos, "left", 0.2)
+
+  pos = imagesearch("./test6.jpg", 0.5)
+  pos = imagesearch("test6.jpg")
+  if pos[0] != -1:
+      click_image("test6.jpg", pos, "left", 0.2)
+
+
+
+
+  pos = imagesearch("./test7.jpg", 0.5)
+  pos = imagesearch("test7.jpg")
+  if pos[0] != -1:
+      click_image("test7.jpg", pos, "left", 0.2)
+
+
+  pos = imagesearch("./test8.jpg", 0.5)
+  pos = imagesearch("./test8.jpg")
+  if pos[0] != -1:
+      click_image("test8.jpg", pos, "left", 0.2)
+
+
+  pos = imagesearch("./test9.jpg", 0.5)
+  pos = imagesearch("./test9.jpg")
+  if pos[0] != -1:
+      click_image("test9.jpg", pos, "left", 0.2)
+
+
+
+
+  pos = imagesearch("./test10.jpg", 0.5)
+  pos = imagesearch("./test10.jpg")
+  if pos[0] != -1:
+      click_image("test10.jpg", pos, "left", 0.2)
+
+
+
+  pos = imagesearch("./test11.jpg", 0.5)
+  pos = imagesearch("./test11.jpg")
+  if pos[0] != -1:
+      click_image("test11.jpg", pos, "left", 0.2) 
+
+
+ 
+  pos = imagesearch("./test12.jpg", 0.5)
+  pos = imagesearch("./test12.jpg")
+  if pos[0] != -1:
+      click_image("test12.jpg", pos, "left", 0.2)
+ 
+
+
+
+  pos = imagesearch("./test13.jpg", 0.5)
+  pos = imagesearch("./test13.jpg")
+  if pos[0] != -1:
+      click_image("test13.jpg", pos, "left", 0.2)
+
+  pos = imagesearch("./test14.jpg", 0.5)
+  pos = imagesearch("./test14.jpg")
+  if pos[0] != -1:
+      click_image("test14.jpg", pos, "left", 0.2)
+
+
+  pos = imagesearch("./test15.jpg", 0.5)
+  pos = imagesearch("./test15.jpg")
+  if pos[0] != -1:
+      click_image("test15.jpg", pos, "left", 0.2)
+
+  pos = imagesearch("./test16.jpg", 0.5)
+  pos = imagesearch("./test16.jpg")
+  if pos[0] != -1:
+      click_image("test16.jpg", pos, "left", 0.2)
+
+  pos = imagesearch("./test17.jpg", 0.5)
+  pos = imagesearch("./test17.jpg")
+  if pos[0] != -1:
+      click_image("test17.jpg", pos, "left", 0.2)
+
+
+
+  pos = imagesearch("./test18.jpg", 0.5)
+  pos = imagesearch("./test18.jpg")
+  if pos[0] != -1:
+      click_image("test18.jpg", pos, "left", 0.2)
+
+
+
+
+  pos = imagesearch("./test19.jpg", 0.5)
+  pos = imagesearch("./test19.jpg")
+  if pos[0] != -1:
+      click_image("test19.jpg", pos, "left", 0.2)
+
+  pos = imagesearch("./test20.jpg", 0.5)
+  pos = imagesearch("./test20.jpg")
+  if pos[0] != -1:
+      click_image("test20.jpg", pos, "left", 0.2)
+
+  
+  pos = imagesearch("./test21.jpg", 0.5)
+  pos = imagesearch("./test21.jpg")
+  if pos[0] != -1:
+      click_image("test21.jpg", pos, "left", 0.2)
+
+  pos = imagesearch("./test22.jpg", 0.5)
+  pos = imagesearch("./test22.jpg")
+  if pos[0] != -1:
+      click_image("test22.jpg", pos, "left", 0.2)
+
+  pos = imagesearch("./test23.jpg", 0.5)
+  pos = imagesearch("./test23.jpg")
+  if pos[0] != -1:
+      click_image("test23.jpg", pos, "left", 0.2)
+
+  pos = imagesearch("./test24.jpg", 0.5)
+  pos = imagesearch("./test24.jpg")
+  if pos[0] != -1:
+      click_image("test24.jpg", pos, "left", 0.2)
+
+  pos = imagesearch("./test25.jpg", 0.5)
+  pos = imagesearch("./test25.jpg")
+  if pos[0] != -1:
+      click_image("test25.jpg", pos, "left", 0.2)
+
+  pos = imagesearch("./test26.jpg", 0.5)
+  pos = imagesearch("./test26.jpg")
+  if pos[0] != -1:
+      click_image("test26.jpg", pos, "left", 0.2)
+
+  pos = imagesearch("./test27.jpg", 0.5)
+  pos = imagesearch("./test27.jpg")
+  if pos[0] != -1:
+      click_image("test27.jpg", pos, "left", 0.2)
+
+  pos = imagesearch("./test28.jpg", 0.5)
+  pos = imagesearch("./test28.jpg")
+  if pos[0] != -1:
+      click_image("test28.jpg", pos, "left", 0.2)
+      n=n+1
+
+  pos = imagesearch("./test29.jpg", 0.5)
+  pos = imagesearch("./test29.jpg")
+  if pos[0] != -1:
+      click_image("test29.jpg", pos, "left", 0.2)
+
+  pos = imagesearch("./test30.jpg", 0.5)
+  pos = imagesearch("./test30.jpg")
+  if pos[0] != -1:
+      click_image("test30.jpg", pos, "left", 0.2)
+
+  pos = imagesearch("./test31.jpg", 0.5)
+  pos = imagesearch("./test31.jpg")
+  if pos[0] != -1:
+      click_image("test31.jpg", pos, "left", 0.2)
+  
+  print("Le bot a fini ",n,"QCM");
+  
+ 
+  
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# AeroBot
- 
+Ce bot est la v1.0 du AeroBot.
+Il est actuellement en bêta_test.
+Pour l'utiliser contacter son auteur : Gabriel Diyan
